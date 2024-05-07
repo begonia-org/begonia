ad8fd35e8fc04a15dfc74fc911ba4049b87ef466
vforfreedom96@gmail.com <vforfreedom96@gmail.com>
2024-05-07 21:34:46 +0800

    feat:use redis/redis-stack-server:latest as redis container

--- internal/biz/gateway/watcher.go
+++ internal/biz/gateway/watcher.go
@@ -36,7 +36,7 @@ func (g *GatewayWatcher) Update(ctx context.Context, key string, value string) e
 	}
 	pd, err := getDescriptorSet(g.config, key, endpoint.DescriptorSet)
 	if err != nil {
-		logger.Log.Errorf("get descriptor set error: %w", err)
+		logger.Log.Errorf("get descriptor set error: %s", err.Error())
 		return errors.New(err, int32(common.Code_INTERNAL_ERROR), codes.Internal, "get_descriptor_set")
 	}
 	err = deleteAll(ctx, pd)
